32a02be9dca2ffca28368160ba7af25f8f12609c
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2020-11-16 11:44:32 +0530

    Redux with react

---
+++ reduxcounter/src/index.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux"
+import counter from "./reducer";
+import App from "./App";
+import "./index.css";
+
+
+var destiantion = document.querySelector("#container");
+
+
+var store = createStore(counter);
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App/>
+</Provider>,
+destiantion
+);
